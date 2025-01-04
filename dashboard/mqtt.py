34c8ae6378d8e4976516b3201725c950958ede68
RABS30 <r.a.b.s35300@gmail.com>
2025-01-04 19:29:36 +0700

    Tambahkan koneksi MQTT

---
+++ dashboard/mqtt.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+   
+# callback saat berhasil terhubung ke server
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("sensor/tanaman")
+    
+
+# callback saat menerima pesan
+def on_message(client, userdata, msg):
+    # print(msg.topic+" "+str(msg.payload))
+    pass
+    
+
+# Konfigurasi MQTT
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+
+# Hubungkan ke broker
+try :
+    mqttc.connect("broker.emqx.io", 1883, 60)
+except Exception as e:
+    print(f'Gagal terhubung ke Broker : {e}')
+    
+mqttc.loop_start()
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import paho.mqtt.client as mqtt 
+
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    context = {
+        'title': 'Dashboard',
+    }
+    return render(request, 'dashboard/dashboard.html', context)
